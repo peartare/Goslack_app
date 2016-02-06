c246c7146dc68341a6da590ee424606d259bbab7
peartare <peartare@users.noreply.github.com>
2016-02-06 10:12:27 -0700

    ch 5 links

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | GoSlack!"
+    assert_select "title", "GoSlack!"
   end
 
   test "should get help" do
@@ -17,6 +17,12 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :about
     assert_response :success
     assert_select "title", "About | GoSlack!"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | GoSlack!"
   end
 
 end
